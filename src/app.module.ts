5616e5887f7a81a4be5bbcdf90ba831cb1542a82
unknown <kushal@talkinglands.com>
2024-03-11 23:26:53 +0530

    Separated DB Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlotModule } from './plot/plot.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/testing'),PlotModule],
+  imports: [PlotModule, DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
